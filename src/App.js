dfdfa3a42badfafb018687e4a1d9dc96012ccb37
JasonDaniel <jasondaniel5295@gmail.com>
2019-09-17 12:39:00 +0530

    changing margin

--- src/App.js
+++ src/App.js
@@ -4,52 +4,10 @@ import './App.css';
 import Chart from './Chart'
 
 const App = () => {
-  const data = [
-    {
-      country: "India",
-      value: 400
-    },
-    {
-      country: "USA",
-      value: 2420
-    },
-    {
-      country: "China",
-      value: 1270
-    },
-    {
-      country: "UK",
-      value: 553
-    },
-    {
-      country: "Germany",
-      value: 731
-    },
-    {
-      country: "Sweden",
-      value: 136
-    },
-    {
-      country: "France",
-      value: 682
-    },
-    {
-      country: "Australia",
-      value: 239
-    },
-    {
-      country: "Canada",
-      value: 367
-    },
-    {
-      country: "Brazil",
-      value: 442
-    }
-  ];
 
         return (
             <div>
-              <Chart chartData = {data}/>
+              <Chart/>
             </div>
           );
       
--- src/Chart.js
+++ src/Chart.js
@@ -13,115 +13,112 @@ import "./App.css";
 const Chart = () => {
   const data = [
     {
-      country: "Vogue",
+      dimBranName: "Vogue",
       value: 400
     },
     {
-      country: "Allure",
+      dimBranName: "Allure",
       value: 2420
     },
     {
-      country: "Teen Vogue",
+      dimBranName: "Teen Vogue",
       value: 1270
     },
     {
-      country: "CN Traveller",
+      dimBranName: "CN Traveller",
       value: 553
     },
     {
-      country: "Wired",
+      dimBranName: "Wired",
       value: 731
     },
     {
-      country: "Bon Appetite",
+      dimBranName: "Bon Appetite",
       value: 136
     },
     {
-      country: "Ars tech",
+      dimBranName: "Ars tech",
       value: 682
     },
     {
-      country: "AD",
+      dimBranName: "AD",
       value: 239
     },
     {
-      country: "The New Yorker",
+      dimBranName: "The New Yorker",
       value: 367
     },
     {
-      country: "Vanity Fair",
+      dimBranName: "Vanity Fair",
       value: 442
     }
   ];
 
   const barRef = useRef();
-   
-  const  width= 1000
-  const height=800
-  const margin = 400
-  const padding = 50
+
+  const width = 1000;
+  const height = 800;
+  const margin = 50;
+  const padding = 50;
 
   //Declaring the entrie code in use effect so that it will be triggered while the page is being loaded
   useEffect(() => {
-
     //referencing the svg where the chart is to be displayed
     const svg = select(barRef.current)
       .attr("width", width)
       .attr("height", height);
 
-    //creacting a group variable 
+    //creacting a group variable
     var g = svg
       .append("g")
-      .attr("transform", "translate(" + 50 + "," + 50 + ")");
+      .attr("transform", "translate("+50 + "," + 0 + ")");
 
     //creating xScale
     const xScale = scaleBand()
-      .domain(data.map(d => d.country))
-      .range([0,width-padding])
-      .padding(0.2)
-      
+      .domain(data.map(d => d.dimBranName))
+      .range([0, width - padding])
+      .padding(0.2);
+
     //creating yScale
     const yScale = scaleLinear()
-      .domain([0,max(data.map(d => d.value))])
-      .range([height-margin,0]);
+      .domain([0, max(data.map(d => d.value))])
+      .range([height - margin, margin]);
 
     //appending xAxis
     g.append("g")
-      .attr("transform", "translate(0," + (height - margin)+ ")")
-      .call(
-          axisBottom(xScale)
-          )
-          .call(g => g.select(".domain")
-    .remove())
-   
+      .attr("transform", "translate(0," + (height - margin) + ")")
+      .call(axisBottom(xScale))
+      .call(g => g.select(".domain").remove())
+      .selectAll("text")
+      .attr("transform","rotate(-65)")
+      .attr("dx")
+     
+
     //appending yAxis
     g.append("g")
-    .call(
-      axisLeft(yScale)
-        .tickFormat(d => {
-          return d + "M"
-        })
-        .ticks(4,"f")
-        .tickSizeInner(-width)      
-    )
-    .call(g => g.select(".domain")
-    .remove())
-    .attr("class","yAxis")
-    .style("text-anchor", "end");
-    
-    //Adding the rectangles
+      .call(
+        axisLeft(yScale)
+          .tickFormat(d => {
+            return d + "M";
+          })
+          .ticks(4)
+          .tickSizeInner(-width)
+      )
+      .call(g => g.select(".domain").remove())
+      .attr("class", "yAxis");
+
+    //Adding the rectangle for bar graphs
     g.selectAll(".bar")
       .data(data)
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr("x", d => xScale(d.country))
+      .attr("x", d => xScale(d.dimBranName))
       .attr("y", d => yScale(d.value))
       .attr("width", xScale.bandwidth())
       .attr("height", d => yScale(0) - yScale(d.value));
   }, [data]);
 
-  
   return (
     <svg width="100%" height="100%" ref={barRef}>
       {" "}
